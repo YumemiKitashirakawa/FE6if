a8e5b93a1fded1a779339a17d639064ec6ce346a
Yumemi_Okazaki <93354884+Congrio1234@users.noreply.github.com>
2026-03-31 01:29:31 -0300

    Add files via upload

--- Project/MAKE_HACK_full.cmd
+++ Project/MAKE_HACK_full.cmd
@@ -10,9 +10,9 @@ set "source_rom=%~dp0FE8_clean.gba"
 
 set "main_event=%~dp0ROMBuildfile.event"
 
-set "target_rom=%~dp0SkillsTest.gba"
-set "target_ups=%~dp0SkillsTest.ups"
-set "target_sym=%~dp0SkillsTest.sym"
+set "target_rom=%~dp0FE6if.gba"
+set "target_ups=%~dp0FE6if.ups"
+set "target_sym=%~dp0FE6if.sym"
 
 @rem defining tools
 
